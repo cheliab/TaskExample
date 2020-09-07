287763a3a413bbe61460053067192b1155366980
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2020-09-08 09:33:48 +1000

    Простой набросок с Task

---
+++ TaskExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    class Program
+    {
+        private static void SimpleTaskExample()
+        {
+            Task task = new Task(() => Console.WriteLine("Hello simple task!"));
+            task.Start();
+
+            Task.Factory.StartNew(() => Console.WriteLine("Hello from factory!"));
+
+            Task.Run(() => Console.WriteLine("Hello from run!"));
+        } 
+        
+        static void Main(string[] args)
+        {
+            SimpleTaskExample();
+            
+            Console.ReadLine();
+        }
+    }
+}
