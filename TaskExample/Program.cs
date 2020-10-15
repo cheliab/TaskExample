a3de86e7b978cdc454227934ec83e1dbd5b7b791
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2020-10-15 10:06:07 +1000

    Добавил пример с вложенными задачами.

--- TaskExample/Program.cs
+++ TaskExample/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskExample
 {
     class Program
     {
+        /// <summary>
+        /// Простой пример запуска задачи 
+        /// </summary>
         private static void SimpleTaskExample()
         {
             Task task = new Task(() => Console.WriteLine("Hello simple task!"));
@@ -13,11 +17,67 @@ namespace TaskExample
             Task.Factory.StartNew(() => Console.WriteLine("Hello from factory!"));
 
             Task.Run(() => Console.WriteLine("Hello from run!"));
-        } 
+        }
+
+        /// <summary>
+        /// Пример с вложенной задачей
+        /// </summary>
+        private static void InnerTaskExample()
+        {
+            // Внешняя задача
+            var outer = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Outer task - Start");
+
+                // Внутренная задача
+                var inner = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Inner task - Start");
+                    Thread.Sleep(5000); // ждем 5 сек.
+                    Console.WriteLine("Inner task - End");
+                });
+                
+                Console.WriteLine("Outer task - End");
+            });
+            outer.Wait();
+            
+            Console.WriteLine("End of Main");
+
+            Console.ReadLine();
+        }
+        
+        /// <summary>
+        /// Пример с вложенной задачей
+        /// </summary>
+        private static void InnerTaskAttachedExample()
+        {
+            // Внешняя задача
+            var outer = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Outer task - Start");
+
+                // Внутренная задача
+                var inner = Task.Factory.StartNew(() =>
+                {
+                    Console.WriteLine("Inner task - Start");
+                    Thread.Sleep(5000); // ждем 5 сек.
+                    Console.WriteLine("Inner task - End");
+                }, TaskCreationOptions.AttachedToParent);
+                
+                Console.WriteLine("Outer task - End");
+            });
+            outer.Wait();
+            
+            Console.WriteLine("End of Main");
+
+            Console.ReadLine();
+        }
         
         static void Main(string[] args)
         {
-            SimpleTaskExample();
+            // SimpleTaskExample();
+            // InnerTaskExample();
+            InnerTaskAttachedExample();
             
             Console.ReadLine();
         }
