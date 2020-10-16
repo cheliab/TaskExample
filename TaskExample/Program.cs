b7f5ad7edd924b6cc7cffd34764b965d672ef0d6
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2020-10-16 11:19:01 +1000

    Добавлены примеры с массивами заданий

--- TaskExample/Program.cs
+++ TaskExample/Program.cs
@@ -17,6 +17,10 @@ namespace TaskExample
             Task.Factory.StartNew(() => Console.WriteLine("Hello from factory!"));
 
             Task.Run(() => Console.WriteLine("Hello from run!"));
+            
+            Console.WriteLine("End of Main");
+
+            Console.ReadLine();
         }
 
         /// <summary>
@@ -72,14 +76,62 @@ namespace TaskExample
 
             Console.ReadLine();
         }
+
+        /// <summary>
+        /// Массив заданий
+        /// </summary>
+        private static void ArrayOfTaskExample()
+        {
+            Task[] taskArr = new Task[3]
+            {
+                new Task(() => Console.WriteLine("First task")),
+                new Task(() => Console.WriteLine("Second task")),
+                new Task(() => Console.WriteLine("Third task"))
+            };
+
+            foreach (var task in taskArr)
+            {
+                task.Start();
+            }
+            
+            // Ждем завершения любой из задач
+            Task.WaitAny(taskArr);
+            
+            Console.WriteLine("Main - end");
+        }
+
+        private static void ArrayOfTaskSecondExample()
+        {
+            Task[] tasks = new Task[3];
+
+            int j = 1;
+            for (int i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Factory.StartNew(() =>
+                {
+                    Thread.Sleep(5000);
+                    Console.WriteLine($"Task {j++}");
+                });
+            }
+            
+            // // Ждать пока завершатся задания
+            // Task.WaitAll(tasks);
+            
+            // Ждем завершения любой задачи
+            Task.WaitAny(tasks);
+
+            Console.WriteLine("Main - end");
+
+            Console.ReadLine();
+        }
         
         static void Main(string[] args)
         {
             // SimpleTaskExample();
             // InnerTaskExample();
-            InnerTaskAttachedExample();
-            
-            Console.ReadLine();
+            // InnerTaskAttachedExample();
+            // ArrayOfTaskExample();
+            ArrayOfTaskSecondExample();
         }
     }
 }
